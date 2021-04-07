355798682c99b5d91c44b7cbb5049ecf73cb9634
Josh Hirner <68080353+jhirner@users.noreply.github.com>
2021-04-07 15:38:19 -0700

    Added 'undo' feature
    Users may now send the message 'undo' to delete the most recent entry from the active Discord channel. Repeatedly sending 'undo' will continue to delete the most recent entry until none remain.

--- source/chonky-discord.py
+++ source/chonky-discord.py
@@ -24,6 +24,30 @@ def update_db(person, weight):
         db[db_table].insert(new_entry)
     return
 
+def delete_prev(person):
+    # When called, find the most recent weight logged
+    # in this channel and delete it.
+    db_table = "weights"
+    
+    query_templ = "SELECT id, weight, MAX(datetime) from {} WHERE person = '{}'" 
+    query_str = query_templ.format(db_table, person)
+    
+    with dataset.connect("sqlite:///" + DB_PATH) as db:
+        # Get the ID number of the most recent entry for the given
+        # person/channel, then delete the dabase entry with that ID.
+        entry_to_del = list(db.query(query_str))[0]
+        id_to_del = entry_to_del["id"]
+        wt_to_del = entry_to_del["weight"]
+        del_from_datetime = entry_to_del["MAX(datetime)"]
+        
+        db[db_table].delete(id = id_to_del)
+        
+        deletion_msg = "Deleted entry: {wt} pounds entered at {dt}".format(
+            wt = wt_to_del,
+            dt = del_from_datetime)
+    
+    return deletion_msg
+
 
 ## Define events.
 
@@ -46,6 +70,8 @@ async def on_message(message):
     if message.author == client.user:
         return
     
+    person_to_update = message.channel.name
+    
     confirmations = ["Message received.",
                      "Understood.",
                      "You betcha.",
@@ -55,18 +81,23 @@ async def on_message(message):
                      "Okay, thanks.",
                      "Affirmative."]
     
-    # Determine if the message is a valid number.
-    # Inform the sender whether the input was accepted.
-    try:
-        new_weight = float(message.content)
-        if new_weight > 0:
-            response = choice(confirmations)
-            person_to_update = message.channel.name
-            update_db(person_to_update, new_weight)
-        else:
-            response = "Negative numbers are not allowed. Neither is zero, you liar."
-    except ValueError:
-        response = "I didn't get that. Please send only numbers."
-    await message.channel.send(response)
+    if message.content == "undo":
+        # Sending 'undo' will delete the most recent entry.
+        delete_msg = delete_prev(person_to_update)
+        await(message.channel.send(delete_msg))
+        
+    else:
+        # Determine if the message is a valid number.
+        # Inform the sender whether the input was accepted.
+        try:
+            new_weight = float(message.content)
+            if new_weight > 0:
+                response = choice(confirmations)                
+                update_db(person_to_update, new_weight)
+            else:
+                response = "Negative numbers are not allowed. Neither is zero, you liar."
+        except ValueError:
+            response = "I didn't get that. Please send only numbers."
+        await message.channel.send(response)
 
 client.run(TOKEN)
